8fe013857d2f8fd30976caedc2a1771d2fea27ad
Brandy Foster <brandy.foster@detroitlabs.com>
2016-01-21 13:24:53 -0500

    fixed location object

--- app/src/main/java/com/detroitlabs/detroitvolunteers/client/models/Location.java
+++ app/src/main/java/com/detroitlabs/detroitvolunteers/client/models/Location.java
@@ -1,6 +1,9 @@
 package com.detroitlabs.detroitvolunteers.client.models;
 
-public class Location {
+import android.os.Parcel;
+import android.os.Parcelable;
+
+public class Location implements Parcelable {
 
     String city;
     String region;
@@ -12,4 +15,34 @@ public class Location {
     public String getRegion(){
         return region;
     }
+
+    public Location() {
+
+    }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeString(this.city);
+        dest.writeString(this.region);
+    }
+
+    protected Location(Parcel in) {
+        this.city = in.readString();
+        this.region = in.readString();
+    }
+
+    public static final Parcelable.Creator<Location> CREATOR = new Parcelable.Creator<Location>() {
+        public Location createFromParcel(Parcel source) {
+            return new Location(source);
+        }
+
+        public Location[] newArray(int size) {
+            return new Location[size];
+        }
+    };
 }
